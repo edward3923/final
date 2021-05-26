ce9b82bab34a251f1a60d8312a2e84b8c301d547
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-05-26 18:54:31 -0500

    initial skeleton v1

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Goal: Kellogg deals main page Javascript
+//
+// Business logic:
+//
+//
+// Tasks:
+// - Create Javascript code that will show an empty index.html page if logged out, and allow user to see data if logged in (week 8)
+// - While logged in, a user should see a "Add a Deal" button that will lead to a the site add-a-deal.html
+// - Will retrieve JSON data from deals.js and will display on the website
+
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
