cb5e4eeb62c4f63c5b08e885e0431236eb919ab4
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-05-26 19:09:36 -0500

    description of deals.js

--- netlify/functions/deals.js
+++ netlify/functions/deals.js
@@ -1,11 +1,23 @@
 // Goal: Kellogg deals API
 //
 // Business logic:
-//
+// - Database should probably have 2 tables - users table and deals table
+// - In the users table, it should store the Identifier, the User UID, the name of the user (if possible)
+// - In the deals table, it should store the ID of the submitter, imgSrc, description, and cost
 //
 // Tasks:
-// Write an API endpiont, using lambda function, that returns information on all the available deals (see courses.js in hw7)
-// 
+// - Write an API endpiont, using lambda function, that returns information on all the available deals (see courses.js in hw7)
+// - I think the structure should look something like:
+// Deals [
+//   {
+//     userName
+//     deal {
+//       imgSrc
+//       description
+//       cost
+//     }
+//   }
+// ]
 
 let firebase = require('./firebase')
 
