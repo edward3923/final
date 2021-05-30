cee62481e1bd1cf35277c123079dd11bc22b4092
Kevin Wilson <kevin.wilson@kellogg.northwestern.edu>
2021-05-30 11:19:02 -0500

    kevin 5.30 11:18

--- add-a-deal.js
+++ add-a-deal.js
@@ -7,3 +7,25 @@
 // - Create Javascript code that will show an empty add-a-deal.html page if logged out, and allow user to see data if logged in (week 8)
 // - While logged in, a user should see several fields he/she can fill out
 // - After filling in the form, the user can click on the "Submit" button and will go back to the index.html page.
+
+firebase.auth().onAuthStateChanged(async function(user) {
+    if (user) {
+      // Signed in
+      console.log('on add deal page')
+
+// get a reference to the submit button
+
+let submitButton = document.querySelector(`#submitbutton`)
+
+// handle the add deal button
+submitButton.addEventListener(`click`, function(event) {
+ event.preventDefault()
+console.log(`submit clicked`)
+  // redirect to the add-a-deal site
+  document.location.href = `index.html`
+  })
+
+} else {
+    // Signed out
+    console.log('signed out')
+}})
--- index.js
+++ index.js
@@ -5,13 +5,43 @@
 //
 // Tasks:
 // - Create Javascript code that will show an empty index.html page if logged out, and allow user to see data if logged in (week 8)
-// - While logged in, a user should see a "Add a Deal" button that will lead to a the site add-a-deal.html
+    // [x] allow user to sign in and out
+// - [x] While logged in, a user should see a "Add a Deal" button that will lead to a the site add-a-deal.html       
 // - Will retrieve JSON data from deals.js and will display on the website
 
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
+
+ // Build the markup for the sign-out button and set the HTML in the header
+ document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+ <button class="text-pink-500 underline sign-out">Sign Out</button>
+`
+
+    // get a reference to the sign out button
+   let signOutButton = document.querySelector(`.sign-in-or-sign-out`)
+
+   // handle the sign out button click
+   signOutButton.addEventListener(`click`, function(event) {
+     // sign out of firebase authentication
+     firebase.auth().signOut()
+
+     // redirect to the home page
+     document.location.href = `index.html`
+   })
+
+// get a reference to the add deal button
+let addDealButton = document.querySelector(`.add-deal-button`)
+
+// handle the add deal button
+addDealButton.addEventListener(`click`, function(event) {
+  event.preventDefault()
+console.log(`add deal clicked`)
+  // redirect to the add-a-deal site
+  document.location.href = `add-a-deal.html`
+})
+
   } else {
     // Signed out
     console.log('signed out')
